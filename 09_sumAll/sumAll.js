1e4a85eafeb530878b982665cd0e356189b8d630
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-06 11:43:44 +0530

    Updated code with answers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,9 +1,17 @@
 const sumAll = function(num1 , num2) {
-    let arr = [];
-    for(let i=num1; i<=num2; i++){
+    if(num1<0 || num2<0) return "ERROR"
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR"
+    if(Number.isNaN(num1) || Number.isNaN(num2)) return "ERROR"
+    if(num1>num2){
+        let temp=num2;
+        num2=num1;
+        num1=temp;
+    }
+    let arr= []
+    for(let i=num1; i<=num2;i++){
         arr.push(i)
     }
-    return arr.reduce((sum,number)=> sum+number,0)
+    return arr.reduce((sum,number)=>sum+number, 0)
 };
 
 // Do not edit below this line
