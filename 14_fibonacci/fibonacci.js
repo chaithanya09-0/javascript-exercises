c2b26fe96fa122d7ddbd29a7df3a1ffa3b8209f5
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-07 22:15:22 +0530

    Updated files

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num = n;
+    if(typeof(n)=== "string") num = Number(n)
+    if ( num===1 || num===2) return 1;
+    if(num===0) return 0;
+    if( num < 0 ) return "OOPS"
+    return fibonacci(num-1) + fibonacci(num-2)
 
 };
 
