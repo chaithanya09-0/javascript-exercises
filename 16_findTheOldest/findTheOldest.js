f2453e38a7b442120e9d9959a586d9233b5f6b83
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-07 23:14:51 +0530

    Updated files

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestAge = 0;
+    let oldestIndex = 0;
+    let ageOfPerson = 0;
+    const currentYear = new Date().getFullYear();
 
+    for (let i = 0; i < arr.length; i++) {
+        const birth = arr[i].yearOfBirth;
+        const death = arr[i].yearOfDeath || currentYear;  // If undefined, use current year
+        ageOfPerson = death - birth;
+
+        if (ageOfPerson > oldestAge) {
+            oldestAge = ageOfPerson;
+            oldestIndex = i;  // ✅ Correct assignment
+        }
+    }
+
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
