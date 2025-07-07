b710d156415354d90d9d877cb450426c9ecea223
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-07 22:06:03 +0530

    Updated files

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr=str.toLowerCase()
+    cleanStr=cleanStr.replace(/[^a-z0-9]/g, '')
+    let arr=cleanStr.split('')
+    let reverse=arr.reverse();
+    reverse = reverse.join('')
+    return (cleanStr === reverse);
 };
 
 // Do not edit below this line
